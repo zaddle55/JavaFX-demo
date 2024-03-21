f3ed29773ac663377f22d0c919bd498dec3a40e2
zaddle55 <12310823@mail.sustech.edu.cn>
2024-03-21 22:44:16 +0800

    update

--- src/main/java/org/example/javafx/HelloController.java
+++ src/main/java/org/example/javafx/HelloController.java
@@ -6,9 +6,25 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private  Label introText;
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    @FXML
+    protected void onExitButtonClick() {
+        System.exit(0);
+    }
+
+    @FXML void onButtonEnter() {
+        introText.setText("You entered the button");
+    }
+    @FXML
+    protected void onClearButtonClick() {
+        introText.setText("");
+    }
+
 }
