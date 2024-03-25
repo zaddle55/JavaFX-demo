c772ba721fb89e361c14d206f8515ce0fde55a12
zaddle55 <12310823@mail.sustech.edu.cn>
2024-03-25 19:06:28 +0800

    update

--- src/main/java/com/zaddle/util/Board.java
+++ src/main/java/com/zaddle/util/Board.java
@@ -1,4 +1,15 @@
-package util;
+package com.zaddle.util;
+/**
+* 定义棋盘逻辑，获胜判断等
+ @ param name 棋盘名称
+ @ param id 棋盘id
+ @ param board 棋盘
+ @ method initBoard 初始化棋盘
+ @ method getBoard 获取棋盘
+ @ method putChess 落子
+ @ method getChess 获取棋子状态
+ @ method judgeWin 判断胜负
+ */
 
 public class Board {
     // 通过name和id来创建一个棋盘
@@ -57,6 +68,47 @@ public class Board {
             return "EMPTY";
         }
     }
-    // 判断是否有一方胜利
-    public static boolean
+    // 判断是否有一方胜利, 1为黑棋胜利，2为白棋胜利，0为未分胜负
+    public static int judgeWin(int x, int y){
+        if (judgeRow(x, y) || judgeCol(x, y) || judgeLeftDiagonal(x, y) || judgeRightDiagonal(x, y)){
+            if (board[x][y] == BLACK){
+                return BLACK;
+            }
+            else if (board[x][y] == WHITE){
+                return WHITE;
+            }
+        }
+    }
+    /*
+    *判断当前列内是否有五子相连
+    @param x 当前横坐标
+    @param y 当前纵坐标
+    @return boolean
+     */
+    public static boolean judgeCol(int x, int y){
+        int count = 1;
+        int color = board[x][y];
+        for (int i = x - 1; i >= 0; i--){
+            if (board[i][y] == color){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        for (int i = x + 1; i < 15; i++){
+            if (board[i][y] == color){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        if (count >= INROW){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
